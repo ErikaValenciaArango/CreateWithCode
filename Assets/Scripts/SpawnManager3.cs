63648acaeabba33ca267a210b40ef9e329aca92a
Erika Valencia Arango <vaerika@unicauca.edu.co>
2024-12-08 20:53:57 -0500

    m

--- Assets/Scripts/SpawnManager3.cs
+++ Assets/Scripts/SpawnManager3.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 public class SpawnManager3 : MonoBehaviour
 {
     public GameObject enemy;
+    private float spawnPos = 9.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(enemy, new Vector3 (0, 0, 6), enemy.transform.rotation);
+        Instantiate(enemy, GenerateSpawnPositon(), enemy.transform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private Vector3 GenerateSpawnPositon()
+    {
+        float spawnPosX = Random.Range(-spawnPos, spawnPos);
+        float spawnPosZ = Random.Range(-spawnPos, spawnPos);
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+        return randomPos;
     }
 }
